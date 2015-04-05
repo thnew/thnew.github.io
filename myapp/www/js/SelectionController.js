93f9b2f15e2d0ebc673092fd8f22523962450692
ThomasHeigl <thnew2@gmail.com>
2015-04-06 01:54:14 +0200

    Added new app design

---
+++ myapp/www/js/SelectionController.js
@@ -0,0 +1,24 @@
+function XSelectionController($scope) {
+	$scope.days = [
+		{
+			date:	"Mon, 7",
+			times: [ "10:00", "14:00" ]
+		},
+		{
+			date:	"Tue, 8",
+			times: [ "10:00", "14:00", "18:00" ]
+		},
+		{
+			date:	"Wed, 9",
+			times: [ "10:00", "14:00", "18:00" ]
+		},
+		{
+			date:	"Thu, 10",
+			times: [ "14:00" ]
+		},
+		{
+			date:	"Fri, 11",
+			times: [ "10:00", "14:00", "18:00" ]
+		},
+	];
+};
