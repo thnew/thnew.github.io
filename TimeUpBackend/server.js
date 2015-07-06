577a9ed0eafeee1c9ae02854d2ee7212245b7b96
Thomas Heigl <thomas.heigl@iteratec.de>
2015-07-06 21:25:48 +0200

    #Common: Added test basics for backend

--- TimeUpBackend/server.js
+++ TimeUpBackend/server.js
@@ -9,14 +9,25 @@
   };
 }
 
+/* Get start parmeters */ {
+  if(process.argv.indexOf("dev") != -1)
+  {
+    var DEV = true;
+  }
+}
+
 /* Config and start server */ {
 	var app = express();
 
 	app.use(bodyParser.json({ limit: "50mb" }));
 
 	app.use(express.static(path.join(__dirname, "../TimeUpFrontend/www")));
-	app.use("/shared", express.static(path.join(__dirname, "../TimeUpShared")));
-
+  app.use("/shared", express.static(path.join(__dirname, "../TimeUpShared")));
+	
+  if(DEV)
+  {
+    app.use("/test", express.static(path.join(__dirname, "../TimeUpTests")));
+  }
 	app.listen(80);
 
 	console.log("Server started :)");
